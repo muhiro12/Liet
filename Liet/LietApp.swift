5f105833ab537c9a83716e28521758dac4cddb92
muhiro_ <muhiro.12@gmail.com>
2026-03-23 19:42:18 +0900

    Initial Commit

---
+++ Liet/LietApp.swift
@@ -0,0 +1,17 @@
+//
+//  LietApp.swift
+//  Liet
+//
+//  Created by Hiromu Nakano on 2026/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
